49bd447942d24df1949e28fb841ec9704e39b128
Abel Ureta-Vidal <abel@sanger.ac.uk>
2004-07-22 11:36:50 +0000

    Added modules, script and example conf file

---
+++ scripts/runtests.pl
@@ -0,0 +1,133 @@
+#!/usr/local/ensembl/bin/perl -w
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+use Test::Harness;
+
+my ($opt_l, $opt_h, $opt_c);
+
+GetOptions('l' => \$opt_l,
+           'h' => \$opt_h,
+	   'c=s' => \$opt_c);
+
+
+#print usage on '-h' command line option
+if($opt_h) {
+  &usage;
+  exit;
+}
+
+#list test files on '-l' command line option
+if($opt_l) {
+   foreach my $file (grep {!/CLEAN\.t/} @{get_all_tests(\@ARGV)}) {
+    print "$file\n";
+  }
+  exit;
+}
+
+#set environment var
+$ENV{'RUNTESTS_HARNESS'} = 1;
+
+#make sure proper cleanup is done if the user interrupts the tests
+$SIG{HUP} = $SIG{KILL} = $SIG{INT} = 
+  sub {warn "\n\nINTERRUPT SIGNAL RECEIVED\n\n"; &clean;};
+
+#run all specified tests
+eval {
+  runtests(grep {!/CLEAN\.t/} @{&get_all_tests(\@ARGV)});
+};
+
+&clean;
+
+sub clean {
+  #unset env var indicating final cleanup should be performed
+  delete $ENV{"RUNTESTS_HARNESS"};
+  if ($opt_c) {
+    eval {
+      runtests(($opt_c));
+    };
+  }
+  exit;
+}
+
+=head2 get_all_tests
+
+  Arg [21]    :(optional) listref $input_files_or_directories
+               testfiles or directories to retrieve. If not specified all 
+               "./" direcroty is the default.
+  Example    : @test_files = read_test_dir('t');
+  Description: Returns a list of testfiles in the directories specified by
+               the @tests argument.  The relative path is given as well as
+               with the testnames returned.  Only files ending with .t are
+               returned.  Subdirectories are recursively entered and the test
+               files returned within them are returned as well.
+  Returntype : listref of strings.
+  Exceptions : none
+  Caller     : general
+
+=cut
+
+sub get_all_tests {
+  my ($input_files_directories) = @_;
+  
+  my @files;
+  my @out = ();
+  my $dir = "";
+
+  if($input_files_directories && @$input_files_directories) {
+    #input files were specified so use them
+    @files = @$input_files_directories
+  } else {
+    #otherwise use every file in the directory
+    $dir = "./t/";
+    unless(opendir(DIR, "./t/")) {
+      warn("WARNING: cannot open directory ./t\n");
+      return [];
+    }
+    @files = readdir DIR;
+    close DIR;
+  }
+
+  #filter out CVS files and ./ and ../
+  @files = grep !/(^\.\.?$)|(^CVS$)/, @files;
+
+  foreach my $file (@files) {
+
+    if(-d $file) {
+      #do a recursive call on directories
+      $file =~ s/\/$//;
+      my @more_files;
+
+      unless(opendir(DIR, $file)) {
+        warn("WARNING: cannot open directory $file\n");
+        return [];
+      }
+      @more_files = map {"$file/$_"} grep { !/(^\.)|(^CVS$)|(~$)/ } readdir DIR;
+      close DIR;
+
+      push @out, @{get_all_tests(\@more_files)};
+
+    } elsif ($file =~ /\.t$/) {
+      #files ending with a '.t' are considered test files
+      # filter out files ending in ~
+      next if ($file =~ /~$/);
+      $file = $dir . $file;
+
+      unless(-r $file && -f $file) {
+	warn("WARNING: cannot read test file $file\n");
+      }
+
+      push @out, $file;
+    } 
+  }
+
+  return \@out;
+}
+
+sub usage {
+  print "usage:\n";
+  print "\tlist tests:        run_tests.pl -l [<testfiles or dirs> ...]\n";
+  print "\trun tests:         run_tests.pl [<testfiles or dirs> ...]\n";
+}
