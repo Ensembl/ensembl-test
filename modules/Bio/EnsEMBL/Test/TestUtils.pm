c60e62c50ef3b815f90bbcff11c2aaeabcec6e7f
Andreas Kusalananda Kähäri <ak4@sanger.ac.uk>
2007-02-07 15:02:20 +0000

    Cleanup

--- modules/Bio/EnsEMBL/Test/TestUtils.pm
+++ modules/Bio/EnsEMBL/Test/TestUtils.pm
@@ -1,6 +1,3 @@
-use strict;
-use warnings;
-
 package Bio::EnsEMBL::Test::TestUtils;
 
 =head1 NAME
@@ -9,9 +6,9 @@ Bio::EnsEMBL::Test::TestUtils - Utilities for testing the EnsEMBL Perl API
 
 =head1 SYNOPSIS
 
-  debug("Testing Bio::EnsEMBL::Slice->foo() method")
-  ok(&test_getter_setter($object, 'foo', 'value'));
-  count_rows($human_dba, "gene");
+    debug("Testing Bio::EnsEMBL::Slice->foo() method");
+    ok( &test_getter_setter( $object, 'foo', 'value' ) );
+    count_rows( $human_dba, "gene" );
 
 =head1 DESCRIPTION
 
@@ -27,17 +24,21 @@ This modules exports the following methods by default:
 
 =head1 CONTACT
 
-Email questions to the ensembl developer mailing list <ensembl-dev@ebi.ac.uk>
+Email questions to the ensembl developer mailing list
+<ensembl-dev@ebi.ac.uk>
 
 =head1 METHODS
 
 =cut
 
+use strict;
+use warnings;
+
 use Exporter;
 
 use vars qw( @ISA @EXPORT );
 
-@ISA = qw(Exporter);
+@ISA    = qw(Exporter);
 @EXPORT = qw(debug test_getter_setter count_rows);
 
 =head2 test_getter_setter
@@ -50,32 +51,34 @@ use vars qw( @ISA @EXPORT );
                The value to use to test the set behavior of the method.
   Example    : ok(&TestUtils::test_getter_setter($object, 'type', 'value'));
   Description: Tests a getter setter method by attempting to set a value
-               and verifying that the newly set value can be retrieved.  The
-               old value of the the attribute is restored after the test 
-               (providing the method functions correctly).
+               and verifying that the newly set value can be retrieved.
+               The old value of the the attribute is restored after the
+               test (providing the method functions correctly).
   Returntype : boolean - true value on success, false on failure
   Exceptions : none
   Caller     : test scripts
 
 =cut
 
-sub test_getter_setter {
-    my ($object, $method, $test_val) = @_;
-    
-    my $ret_val = 0;
-    
-    #save the old value
-    my $old_val = $object->$method;
-    
-    $object->$method($test_val);
-    
-    #verify value was set
-    $ret_val = (!defined($test_val) && !defined($object->$method)) || 
-	       ($object->$method eq $test_val);
+sub test_getter_setter
+{
+    my ( $object, $method, $test_val ) = @_;
 
-    #restore the old value
+    my $ret_val = 0;
+
+    # Save the old value
+    my $old_val = $object->$method();
+
+    $object->$method($test_val);
+
+    # Verify value was set
+    $ret_val =
+      (      ( !defined($test_val) && !defined( $object->$method() ) )
+          || ( $object->$method() eq $test_val ) );
+
+    # Restore the old value
     $object->$method($old_val);
-    
+
     return $ret_val;
 }
 
@@ -91,10 +94,11 @@ sub test_getter_setter {
 
 =cut
 
-sub debug {
-  if( $::verbose ) {
-    print STDERR @_,"\n";
-  }
+sub debug
+{
+    if ($::verbose) {
+        print STDERR @_, "\n";
+    }
 }
 
 =head2 count_rows
@@ -109,14 +113,18 @@ sub debug {
 
 =cut
 
-sub count_rows {
-  my $db = shift;
-  my $tablename = shift;
+sub count_rows
+{
+    my $db        = shift;
+    my $tablename = shift;
 
-  my $sth = $db->dbc->prepare( "select count(*) from $tablename" );
-  $sth->execute();
-  my ( $count ) = $sth->fetchrow_array();
-  return $count;
+    my $sth = $db->dbc->prepare("select count(*) from $tablename");
+
+    $sth->execute();
+
+    my ($count) = $sth->fetchrow_array();
+
+    return $count;
 }
 
 1;
