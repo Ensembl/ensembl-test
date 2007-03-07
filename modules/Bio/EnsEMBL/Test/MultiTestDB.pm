b4888440c3a5ac131d785b850055b8fa577cd0f5
Glenn Proctor <gp1@sanger.ac.uk>
2007-03-07 14:12:05 +0000

    Fixed typo causing error message on first run with an already unpacked database.

--- modules/Bio/EnsEMBL/Test/MultiTestDB.pm
+++ modules/Bio/EnsEMBL/Test/MultiTestDB.pm
@@ -376,7 +376,7 @@ sub unzip_test_dbs
     my ( $self, $zipfile ) = @_;
 
     if ( -e $self->curr_dir() . '/' . DUMP_DIR ) {
-        warning( "# !! '" . $self->curr_dir() . '/' . DUMP_DIR,
+        warning( "# !! '" . $self->curr_dir() . '/' . DUMP_DIR.
             "' already unpacked\n" );
         return;
     }
