98061f17b1dd2149725f07d54c0387bb079b0f8a
Michael Gray <mg13@sanger.ac.uk>
2013-10-11 10:03:34 +0000

    convert_test_schemas.sh: auto-discover databases to convert.

--- scripts/convert_test_schemas.sh
+++ scripts/convert_test_schemas.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 script_dir=$( cd $( dirname $0 ); echo $PWD )
-test_dir=${1:-modules/t}
+test_dir=${1:-$PWD/modules/t}
 
 if [ ! -d "${test_dir}" ]; then
     echo "Cannot find: ${test_dir}"
@@ -31,12 +31,17 @@ convert_schema() {
   echo
 }
 
-for db_type in core empty; do
-  convert_schema 'homo_sapiens' "${db_type}"
-done
-
-convert_schema 'circ' 'core'
-convert_schema 'ontology' 'ontology'
+(
+    cd "${test_dir}/test-genome-DBs"
+    for species in *; do
+        (
+            cd "${species}"
+            for db_type in *; do
+                convert_schema "${species}" "${db_type}"
+            done
+        )
+    done
+)
 
 exit 0
 
