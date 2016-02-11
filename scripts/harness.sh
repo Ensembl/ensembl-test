ed42142f3a37f534ef1c047e5f067288d5588775
Matthew Laird <lairdm@ebi.ac.uk>
2016-02-11 15:19:38 +0000

    - Adding a generic testing harness that's not tied to Travis

---
+++ scripts/harness.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+join_array() { local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}"; }
+
+# Find some initial paths, where is this script,
+# what is the repos' parent directory, and where
+# are our dependencies installed
+HARNESS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PPWD="${PWD}/.."
+ENSDIR="${ENSDIR:-$PPWD}"
+setenv="$ENSDIR/ensembl/activate"
+
+# Setup the paths and perl5lib
+$setenv -vv $ENSDIR
+if [ "$ENSDIR" != "$PPWD" ]; then
+    $setenv -vvd $PWD
+fi
+
+export TEST_AUTHOR=$USER
+
+# If there's a database configuration for this build type, link
+# it in to place
+if [ -f "modules/t/MultiTestDB.conf.$DB" ]; then
+    (cd modules/t && ln -sf MultiTestDB.conf.$DB MultiTestDB.conf)
+fi
+
+# Build the PERL5OPT and SKIP_TESTS based on the environment
+MATRIX=( "" "_$DB" "_COVERALLS_$COVERALLS" )
+PERL5OPT_array=()
+SKIP_TESTS_array=()
+
+for h in "${MATRIX[@]}"
+do
+
+    PERL5OPT_var="PERL5OPT$h"
+    if [ ! -z ${!PERL5OPT_var} ]; then
+	PERL5OPT_array+=(${!PERL5OPT_var})
+    fi
+
+    SKIP_TESTS_var="SKIP_TESTS$h"
+    if [ ! -z ${!SKIP_TESTS_var} ]; then
+	SKIP_TESTS_array+=(${!SKIP_TESTS_var})
+    fi
+done
+
+if [ ${#PERL5OPT_array[@]} -ne 0 ]; then
+    PERL5OPT=$(join_array ' ' ${PERL5OPT_array[@]})
+    echo "Using PERL5OPT=$PERL5OPT"
+fi
+
+if [ ${#SKIP_TESTS_array[@]} -ne 0 ]; then
+    SKIP_TESTS='--skip '
+    SKIP_TESTS+=$(join_array ',' ${SKIP_TESTS_array[@]})
+fi
+
+echo "Running test suite"
+echo "Executing: perl $ENSDIR/ensembl-test/scripts/runtests.pl modules/t $SKIP_TESTS"
+perl $ENSDIR/ensembl-test/scripts/runtests.pl modules/t $SKIP_TESTS
+
+rt=$?
+if [ $rt -eq 0 ]; then
+  if [ "$COVERALLS" = 'true' ]; then
+    echo "Running Devel::Cover coveralls report"
+    cover --nosummary -report coveralls
+  fi
+  exit $?
+else
+  exit $rt
+fi
