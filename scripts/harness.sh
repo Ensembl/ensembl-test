36aaf715d9a434f8cc3632f080ed9570e2dc63e7
Matthew Laird <lairdm@ebi.ac.uk>
2016-02-12 13:49:48 +0000

    qUOTING ISSUE AROUND BUILDING perl5opt VARIABLE IN THE HARNESS

--- scripts/harness.sh
+++ scripts/harness.sh
@@ -44,7 +44,7 @@ do
 done
 
 if [ ${#PERL5OPT_array[@]} -ne 0 ]; then
-    PERL5OPT=\'$(join_array ' ' ${PERL5OPT_array[@]})\'
+    PERL5OPT=$(join_array ' ' ${PERL5OPT_array[@]})
     export PERL5OPT
     echo "Using PERL5OPT=$PERL5OPT"
 fi
